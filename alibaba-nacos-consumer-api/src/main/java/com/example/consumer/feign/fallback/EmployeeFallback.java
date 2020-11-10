5e4aa455fa235b57ae7c44149c4d22f65fd2564b
zhang <zhanghx6@asiainfo.com>
2020-11-10 18:11:04 +0800

    修改工程和模块名称

---
+++ alibaba-nacos-consumer-api/src/main/java/com/example/consumer/feign/fallback/EmployeeFallback.java
@@ -0,0 +1,25 @@
+package com.example.consumer.feign.fallback;
+
+import com.example.consumer.feign.api.EmployeeFeignService;
+import feign.hystrix.FallbackFactory;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName EmployeeFallback
+ * @User zhang
+ * @Description Feign接口调用失败的回调方法
+ * @Author Lucien
+ * @Date 2020/8/30 0:07
+ * @Version 1.0
+ */
+@Log4j2
+@Component
+public class EmployeeFallback implements FallbackFactory<EmployeeFeignService> {
+
+    @Override
+    public EmployeeFeignService create(Throwable throwable) {
+        log.error("EmployeeFeignService接口调用异常:{}", throwable.getMessage());
+        return empName -> null;
+    }
+}
